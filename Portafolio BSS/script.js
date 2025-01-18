4a7fc739db45c9a4c2255ba667bf8d509bd0f579
Desh22 <109133831+Desh22@users.noreply.github.com>
2025-01-18 18:11:03 -0300

    Logo y portafolio DB Soft

--- Portafolio BSS/script.js
+++ Portafolio BSS/script.js
@@ -1,23 +1,6 @@
-// Función para detectar cuándo una sección está en el viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return rect.top <= window.innerHeight && rect.bottom >= 0;
-}
+const darkModeBtn = document.getElementById('darkModeBtn');
+const body = document.body;
 
-// Agregar clase 'visible' a las secciones en el viewport y 'hidden' al salir
-function checkSectionsVisibility() {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        if (isInViewport(section)) {
-            section.classList.add('visible');
-            section.classList.remove('hidden');
-        } else {
-            section.classList.remove('visible');
-            section.classList.add('hidden');
-        }
-    });
-}
-
-// Ejecutar la función al cargar y al hacer scroll
-window.addEventListener('scroll', checkSectionsVisibility);
-window.addEventListener('load', checkSectionsVisibility);
+darkModeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
